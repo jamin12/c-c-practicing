7c8f97a4525a3575b4dddd60f5d88b1bb9ca8d92
jamin <rudals9901@naver.com>
2021-04-28 22:19:03 +0900

    .

--- helloworld_C/hello.c
+++ helloworld_C/hello.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
-int main(){
-    printf("hello, world!\n");
+int main()
+{
+    int a = 1, b = 1, c = 0, i;
+    for (i = 0; i <= 10; i++)
+    {
+        c = a + b;
+        if (c < 100)
+        {
+
+            
+            printf("%d", c);
+        }
+        a = b;
+        b = c;
+    }
     return 0;
+}
